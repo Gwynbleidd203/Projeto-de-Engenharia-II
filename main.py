555f85018f5262af24bb63b76aa4b8cbd88f1f25
Otavio Augusto Barbieri <15821087651@MUZAMBINHO.EDU.BR>
2022-03-28 22:17:50 -0300

    Projeto Versão 1.00

---
+++ main.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, request, redirect, session, flash
+
+app = Flask(__name__)
+app.secret_key = 'ENGII'
+
+class Tarefa:
+    def __init__(self, nome, categoria):
+        self._nome = nome
+        self._categoria = categoria
+
+tarefa01 = Tarefa('Estudar', 'Estudos')
+tarefa02 = Tarefa('Compras', 'Dia-a-dia')
+tarefa03 = Tarefa('Jogar LOL', 'Alta importância')
+
+lista = [tarefa01, tarefa02, tarefa03]
+
+@app.route('/')
+def index():
+    return render_template('index.html', tarefas=lista)
+
+
+if __name__ == '__main__':
+
+    app.run(debug=True)
+
+        
